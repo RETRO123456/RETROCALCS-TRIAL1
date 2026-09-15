657a70e079825973db4046809fd3f0331d22264c
RETRO123456 <jethroespineda.pdelta@gmail.com>
2026-09-15 08:07:31 +0800

    Add files via upload

---
+++ assets/nav.js
@@ -0,0 +1,28 @@
+(function(){
+  var CALCS = [
+    { href: '../calculators/slab-flexure.html', label: 'Slab Flexure' },
+    { href: '../calculators/beam-flexure.html', label: 'Beam Flexure' },
+    { href: '../calculators/beam-shear.html', label: 'Beam Shear' },
+    { href: '../calculators/column-axial-bending.html', label: 'Column Axial + Bending' }
+  ];
+
+  var path = window.location.pathname;
+  var here = path.substring(path.lastIndexOf('/') + 1);
+
+  var links = CALCS.map(function(c){
+    var file = c.href.substring(c.href.lastIndexOf('/') + 1);
+    var active = (file === here);
+    return '<a href="' + c.href + '" class="site-nav-link' + (active ? ' active' : '') + '">' + c.label + '</a>';
+  }).join('');
+
+  var navHtml =
+    '<nav class="site-nav">' +
+      '<a href="../index.html" class="site-nav-brand">' +
+        '<img src="../assets/retrocrete-logo.jpg" alt="RetroCrete FC">' +
+        '<span>RETROCRETE FC</span>' +
+      '</a>' +
+      '<div class="site-nav-links">' + links + '</div>' +
+    '</nav>';
+
+  document.body.insertAdjacentHTML('afterbegin', navHtml);
+})();
